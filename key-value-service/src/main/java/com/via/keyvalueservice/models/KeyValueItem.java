850546d82a3bfbbaea0d8f94bea3ee72c60fbcc1
Atanas <apleced@gmail.com>
2020-04-09 17:13:46 +0300

    Switch to using mongodb

--- key-value-service/src/main/java/com/via/keyvalueservice/models/KeyValueItem.java
+++ key-value-service/src/main/java/com/via/keyvalueservice/models/KeyValueItem.java
@@ -3,14 +3,11 @@ package com.via.keyvalueservice.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class KeyValueItem {
     private @Id String key;
     private String value;
--- key-value-service/src/main/java/com/via/keyvalueservice/models/KeyValueItemRepository.java
+++ key-value-service/src/main/java/com/via/keyvalueservice/models/KeyValueItemRepository.java
@@ -1,6 +1,6 @@
 package com.via.keyvalueservice.models;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface KeyValueItemRepository extends JpaRepository<KeyValueItem, String> {
+public interface KeyValueItemRepository extends MongoRepository<KeyValueItem, String> {
 }
