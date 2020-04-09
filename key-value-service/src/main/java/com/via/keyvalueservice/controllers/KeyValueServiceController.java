edbeb6978f97175515790626ea47a98a1cddd24f
Atanas <apleced@gmail.com>
2020-04-09 22:33:36 +0300

    Add optional paging for test purposes. Add test initialization of the db

--- key-value-service/src/main/java/com/via/keyvalueservice/controllers/KeyValueServiceController.java
+++ key-value-service/src/main/java/com/via/keyvalueservice/controllers/KeyValueServiceController.java
@@ -2,6 +2,8 @@ package com.via.keyvalueservice.controllers;
 
 import com.via.keyvalueservice.models.KeyValueItem;
 import com.via.keyvalueservice.models.KeyValueItemRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -47,9 +50,16 @@ public class KeyValueServiceController {
         keyValueItemRepository.deleteAll();
     }
 
+    //getAll takes an optional page number parameter "p" which selects key value items in batches of 500
+    //if the parameter is not supplied the whole data is fetched at once.
     @GetMapping("/getAll")
-    List<KeyValueItem> getAll() {
-        return keyValueItemRepository.findAll();
+    List<KeyValueItem> getAll(@RequestParam(name = "p") Optional<Integer> pageNumber) {
+        if(pageNumber.isPresent()) {
+            Pageable pageable = PageRequest.of(pageNumber.get(), 500);
+            return keyValueItemRepository.findAll(pageable).getContent();
+        } else {
+            return keyValueItemRepository.findAll();
+        }
     }
 
     @GetMapping("/getKeys")
